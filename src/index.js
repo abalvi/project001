5450207e77d81f0537b98ec298b729e03a37f262
abalvi <abalvi@gmail.com>
2020-06-09 20:37:18 +0500

    added changes to the app.js file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="Alvi" age = {40}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
